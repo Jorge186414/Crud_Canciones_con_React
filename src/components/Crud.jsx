9d3fecffaa647a357546906c4f6043c569070128
Jorge Miranda Zuniga <jorgaismiranda@gmail.com>
2024-05-21 08:31:44 -0600

    Modified: Ordenamiento del formulario y del mapeo de los objetos

--- src/components/Crud.jsx
+++ src/components/Crud.jsx
@@ -15,7 +15,7 @@ function Crud() {
                 title: "Canciones Tesji",
                 text: "Debes ingresar un nombre de cancion",
                 icon: "error"
-              });
+            });
             return
         }
         if (!artista.trim()) {
@@ -23,7 +23,7 @@ function Crud() {
                 title: "Canciones Tesji",
                 text: "Debes ingresar un artista de cancion",
                 icon: "error"
-              });
+            });
             return
         }
         if (!genero.trim()) {
@@ -31,7 +31,7 @@ function Crud() {
                 title: "Canciones Tesji",
                 text: "Debes ingresar un genero para la cancion",
                 icon: "error"
-              });
+            });
             return
         }
         console.log(`Procesando datos... 
@@ -55,37 +55,41 @@ function Crud() {
         <>
             <div className="container border border-primary p-4 mt-3 rounded-3 text-center">
                 <h1>Registro de Canciones</h1>
-                <form onSubmit={guardarTrack}>
-                    <input
-                        type="text"
-                        placeholder='Track'
-                        className='form-control mb-3'
-                        onChange={(evt) => setTrack(evt.target.value)}
-                    />
-                    <input
-                        type="text"
-                        placeholder='Artista'
-                        className='form-control mb-3'
-                        onChange={(evt) => setArtista(evt.target.value)}
-                    />
-                    <input
-                        type="text"
-                        placeholder='Genero'
-                        className='form-control mb-3'
-                        onChange={(evt) => setGenero(evt.target.value)}
-                    />
-                    <button type='submit' className='btn btn-success' >Agregar Cancion</button>
-                </form>
-                <hr />
-                <h3>Lista de canciones</h3>
-                <ol>{
-                    canciones.map((cancion, index) => (
-                        <li key={index}>{cancion.cancion}, {cancion.artist}, {cancion.gen}</li>
-                    ))
-                }
-                </ol>
+                <div className="row">
+                    <div className='col-sm'>
+                        <form onSubmit={guardarTrack}>
+                            <input
+                                type="text"
+                                placeholder='Track'
+                                className='form-control mb-3'
+                                onChange={(evt) => setTrack(evt.target.value)}
+                            />
+                            <input
+                                type="text"
+                                placeholder='Artista'
+                                className='form-control mb-3'
+                                onChange={(evt) => setArtista(evt.target.value)}
+                            />
+                            <input
+                                type="text"
+                                placeholder='Genero'
+                                className='form-control mb-3'
+                                onChange={(evt) => setGenero(evt.target.value)}
+                            />
+                            <button type='submit' className='btn btn-success' >Agregar Cancion</button>
+                        </form>
+                    </div>
+                    <div className='col-sm'>
+                        <h3>Lista de canciones</h3>
+                        <ol>{
+                            canciones.map((cancion, index) => (
+                                <li key={index}>{cancion.cancion}, {cancion.artist}, {cancion.gen}</li>
+                            ))
+                        }
+                        </ol>
+                    </div>
+                </div>
             </div>
-
         </>
     )
 }
